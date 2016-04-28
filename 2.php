a5c27c1f855d4f14a4e6fea714b338ce2b6777b3
george1028 <george10282006@hotmail.com>
2016-04-28 12:55:05 +0800

    ddd

--- 2.php
+++ 2.php
@@ -8,6 +8,7 @@ $i = 0;
 if (isset($_POST['search'])){
         $searchq = $_POST['search'];
        # $searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
+        $searchq = str_replace(' ','+',$searchq);
         $query = mysql_query("select * from server where `FQDN` like '%$searchq%'") or die(mysql_error());
         $count = mysql_num_rows($query);
         if($count == 0){
@@ -42,31 +43,23 @@ $lastPos = 0;
 $positions = array(); 
 $result = array();
 echo '<h3>'.$count.' records retrieved.</h3>';
-$content = strip_tags(file_get_contents("https://www.google.com.au/search?client=aff-cs-360se&ie=UTF-8&q=perth+jobs&"));
-$content1 = file_get_contents("https://www.google.com.au/search?client=aff-cs-360se&ie=UTF-8&q=perth+jobs&");
+#$content = strip_tags(file_get_contents("https://www.google.com.au/search?client=aff-cs-360se&ie=UTF-8&q=perth+jobs&"));
+#$content1 = file_get_contents("https://www.google.com.au/search?client=aff-cs-360se&ie=UTF-8&q=perth+jobs&");
+$content1 = file_get_contents("https://www.google.com.au/search?q=".$searchq."&");
 if($searchq != ''){
-        while (($lastPos = stripos($content1, $searchq, $lastPos))!== false) {
+        while (($lastPos = stripos($content1, 'http', $lastPos))!== false) {
     $positions[] = $lastPos;
-    #$result[] = substr($content, $lastPos-20, 50);
-    $temp = substr($content1, $lastPos, 130);
-   /* $postart = stripos($temp,' ');
-    $temp = substr($temp, $postart);
-    $posend = strripos($temp,' ');
-    $temp = substr($temp,0, $posend);
-    $pos=stripos($temp,$searchq);
-    $result[] = '...'.substr($temp,0,$pos).'<b>'.substr($temp,$pos,strlen($searchq)).'</b>'.substr($temp,$pos+strlen($searchq)).'...';
-   */
-   if(stripos($temp,'&amp'))
-   $result[] = substr($temp,0,stripos($temp,'&amp'));
-else if(stripos($temp,'//')) 
-   $result[] = $temp;
+    $temp = substr($content1, $lastPos-30, 160);
+   if(stripos($temp,'&amp') && stripos($temp,'class="r"'))
+   $result[] = substr($temp,stripos($temp,'http'),stripos($temp,'&amp')-stripos($temp,'http'));
+#else if(stripos($temp,'//')) $result[] = $temp;
    $lastPos = $lastPos + strlen($searchq);
 }
     echo sizeof($positions)." occurrences are found for '$searchq' <br /><br /><br />";
 foreach ($result as $display){
         echo '<font size=5 color=#000>'.htmlspecialchars($display)."</font> <br /><br />";
 }
-#echo htmlspecialchars("<meta content");
+#echo $searchq;
     /*   # if (strstr($content,$searchq)) {
          if($pos=stripos($content,$searchq)){
                  $rest = substr($content, $pos-20, 50);
